7afb0e2296deb80bedbf7d00175c3df1189dcf94
Aaron Whitehead <whitehead.aaron@live.com>
2019-02-28 16:31:05 -0700

    finished names liking challenge

--- activeGitHub.js
+++ activeGitHub.js
@@ -36,9 +36,36 @@ function printerError(s) {
 function isTriangle(a, b, c) {
   let newArr = [a, b, c].sort();
   if (newArr[0] + newArr[1] <= newArr[2]) {
-      return false;
+    return false;
   }
   return true;
 }
 
 // console.log(isTriangle(1, 2, 2));
+
+// Write a function that counts and returns a statement based on how many people "like" something on facebook
+// likes [] // must be "no one likes this"
+// likes ["Peter"] must be "Peter likes this"
+// likes ["Jacob", "Alex"] must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] must be "Max, John and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] must be "Alex, Jacob and 2 others like this"
+
+function likes(names) {
+  if (names.length === 0) {
+      return 'no one likes this';
+  }
+  if (names.length === 1) {
+      return `${names[0]} likes this`;
+  }
+  if (names.length === 2) {
+      return `${names[0]} and ${names[1]} like this`
+  }
+  if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  }
+  else {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+};
+
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
