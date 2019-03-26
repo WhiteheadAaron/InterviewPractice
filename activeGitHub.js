b5f37769becc07c2c5e358866da22f6685b9d3ec
Aaron Whitehead <whitehead.aaron@live.com>
2019-03-26 12:04:25 -0700

    finishing drills

--- activeGitHub.js
+++ activeGitHub.js
@@ -547,7 +547,7 @@ function crackingTheCode(input) {
 
 
 
-function daysInMonth(str, leap) {
+function daysInMonth(str, leap = false) {
 
   let answer = "";
 
@@ -581,3 +581,49 @@ function rockPaperScissors(input, compNumb = Math.floor(Math.random() * 3) + 1) 
 }
 
 // console.log(rockPaperScissors('Rock'));
+
+
+
+function repeat(fn, n) {
+  for (let i = 0; i < n; i++) {
+    fn();
+  }
+}
+
+function helloWorld() {
+  console.log('Hello World');
+}
+
+function goodbyeWorld() {
+  console.log('Goodbye World');
+}
+
+// console.log(repeat(helloWorld, 5));
+
+
+
+function filter(arr, fn) {
+  // TASK: Define your function here
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])) {
+      newArr.push(arr[i]);
+    } 
+  }
+  return newArr;
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
+
+// console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
